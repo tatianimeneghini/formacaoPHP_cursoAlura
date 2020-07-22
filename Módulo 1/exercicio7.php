0bfe81336a9b60717fe055653ed46f2f4df2824a
tatianimeneghini <tatiani.meneghini@gmail.com>
2020-07-21 22:25:01 -0300

    atualização README e exercício 7

--- Módulo 1/exercicio7.php
+++ Módulo 1/exercicio7.php
@@ -1,8 +1,12 @@
 <?php
 
+$nome = "Tatiani";
 $idade = 29;
 echo "Você só pode entrar se tiver mais que 18 anos" .PHP_EOL;
 
-if ($idade > 18) {
-    echo "Você tem $idade anos. Pode entrar!";
+if ($idade >= 18 and $nome = "Tatiani") {
+    echo "Você tem $idade anos". PHP_EOL;
+    echo "Pode entrar!";
 }
+
+echo "Você só tem $idade anos. Você não pode entrar";
